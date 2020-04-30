09938b11eaeb5fb7593e8f9459dfa14c18278f4b
Kim Chouard <k.chouard@salesforce.com>
2020-04-30 09:41:40 -0700

    Remove old behavior

--- app/src/scripts/content/components/app.jsx
+++ app/src/scripts/content/components/app.jsx
@@ -18,40 +18,7 @@ class App extends Component {
     super(props);
   }
 
-  checkSid() {
-    if (location.href.search(apiUrl) !== -1 && location.pathname === '/loggedIn') {
-      console.log('API website detected.');
-
-      if (!this.props.player) {
-        this.props.logIn();
-      }
-      else {
-        let messageDiv = document.getElementById('message');
-        messageDiv.innerHTML = 'Loading...';
-        
-        let count = 3;
-        let intId = setInterval((() => {
-          if (count === 0) {
-            messageDiv.innerHTML = 'Redirecting to new tab...';
-            clearInterval(intId);
-            // Used to redirect to new tab
-            this.props.unselectQuest(true);
-          }
-          else {
-            messageDiv.innerHTML = `Redirecting to a new tab in ${ count } seconds`;
-            count--;
-          }
-        }).bind(this), 1000);
-
-        setTimeout(() => {
-          messageDiv.innerHTML = 'Open a new tab to start the game!';
-        }, 2000*count);
-      }
-    }
-  }
-
   render() {
-    this.checkSid();
     return (
       <div>
         { (this.props.activeQuest && this.props.activeQuestData) ? <Bubble embed={true}><Accordions stages={ this.props.activeQuestData.stages }/></Bubble> : null }
