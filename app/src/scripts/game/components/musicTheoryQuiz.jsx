5d3c7f65b6dec73292a044f5121e3549874e3666
Kim Chouard <k.chouard@salesforce.com>
2020-08-19 16:59:15 -0700

    Adding `musictheory` question type for Quiz

---
+++ app/src/scripts/game/components/musicTheoryQuiz.jsx
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getActiveQuestData } from '../../_utils';
+import { } from '../../../actions/index';
+
+
+class MusicTheoryQuiz extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { }
+  }
+
+  render() {
+    if (this.props.question && this.props.activeQuestData) {
+      return <div className="musicTheoryQuizWrapper">
+        <h4>{ this.props.question.name }</h4>
+        <p>This is an interactive quiz! :D</p>
+      </div>
+    }
+    else {
+      return <div>Loading...</div>
+    }
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    embeddedPage: state.embeddedPage,
+    activeQuest: state.activeQuest,
+    activeQuestData: getActiveQuestData(state),
+  };
+};
+
+function mapDispatchToProps(dispatch) {
+return bindActionCreators({ }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MusicTheoryQuiz);
