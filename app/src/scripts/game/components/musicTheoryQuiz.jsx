5fad9dba3885ffdf059d4d2f858d1d8a4f8392b0
Kim Chouard <k.chouard@salesforce.com>
2020-08-20 12:50:18 -0700

    Piano working with no sound

--- app/src/scripts/game/components/musicTheoryQuiz.jsx
+++ app/src/scripts/game/components/musicTheoryQuiz.jsx
@@ -10,6 +10,7 @@ import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getActiveQuestData } from '../../_utils';
 import { } from '../../../actions/index';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
 
 
 class MusicTheoryQuiz extends Component {
@@ -21,9 +22,31 @@ class MusicTheoryQuiz extends Component {
 
   render() {
     if (this.props.question && this.props.activeQuestData) {
+      const firstNote = MidiNumbers.fromNote('c3');
+      const lastNote = MidiNumbers.fromNote('f5');
+      const keyboardShortcuts = KeyboardShortcuts.create({
+        firstNote: firstNote,
+        lastNote: lastNote,
+        keyboardConfig: KeyboardShortcuts.HOME_ROW,
+      });
+
       return <div className="musicTheoryQuizWrapper">
         <h4>{ this.props.question.name }</h4>
         <p>This is an interactive quiz! :D</p>
+
+        <Piano
+          noteRange={{ first: firstNote, last: lastNote }}
+          playNote={(midiNumber) => {
+            // Play a given note - see notes below
+            console.log('Playing note:', midiNumber);
+          }}
+          stopNote={(midiNumber) => {
+            // Stop playing a given note - see notes below
+            console.log('Stop playing note:', midiNumber);
+          }}
+          width={1000}
+          keyboardShortcuts={keyboardShortcuts}
+        />
       </div>
     }
     else {
